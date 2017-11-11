a1c8ae4ef7f0871b406d05c1dc78d0d7e1ca2ebe
Joabe Costa <joabetc@hotmail.com>
2017-11-11 09:51:14 -0200

    Working with Date object

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,9 +10,21 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
+    let data = new Date(this._inputData.value.split('-'));
+    // or
+    // let data = new Date(this._inputData.replace(/-/, ','));
+
     console.log(typeof(this._inputData.value));
 
     console.log(this._inputData.value);
+
+    let negociacao = new Negociacao(
+      this._inputData.value,
+      this._inputQuantidade,
+      this._inputValor
+    );
+
+    console.log(negociacao);
   }
 
 }
