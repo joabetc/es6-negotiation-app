4f2a7685d788b75e2da454fbe33ef9aca0008df8
Joabe Costa <joabetc@hotmail.com>
2017-11-10 22:18:06 -0200

    Adding methods for other private attributes

--- client/js/app/models/Conta.js
+++ client/js/app/models/Conta.js
@@ -1,17 +1,25 @@
 class Conta {
   
       constructor(titular, conta) {
-          this.titular = titular;
-          this.conta = conta;
+          this._titular = titular;
+          this._conta = conta;
           this._saldo = 0.0
       }
   
       deposita(valor) {
           console.log('Valor depositado: ' + valor);
-          this._saldo+=valor; 
+          this._saldo += valor; 
       }
   
       getSaldo() {
           return this._saldo;
+      }
+
+      getTitular() {
+        return this._titular
+      }
+
+      getConta() {
+        return this._conta;
       }
   }
