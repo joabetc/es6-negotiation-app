95a8057ab06ebd23abf80d31ac09edd5e66aed16
Joabe Costa <joabetc@hotmail.com>
2017-11-11 16:41:46 -0200

    Creating a dynamic template with map function

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -4,7 +4,7 @@ class NegociacoesView {
       this._element = element;
     }
 
-    _template() {
+    _template(model) {
       return `
         <table class="table table-hover table-bordered">
           <thead>
@@ -17,6 +17,16 @@ class NegociacoesView {
           </thead>
           
           <tbody>
+            ${model.negociacoes.map(n => {
+              return `
+                <tr>
+                  <td>${DateHelper.dataParaTexto(n.data)}</td>
+                  <td>${n.quantidade}</td>
+                  <td>${n.valor}</td>
+                  <td>${n.volume}</td>
+                </tr>
+              `
+            }).join('')}
           </tbody>
           
           <tfoot>
@@ -25,7 +35,7 @@ class NegociacoesView {
       `;
     }
 
-    update() {
-      this._element.innerHTML = this._template();
+    update(model) {
+      this._element.innerHTML = this._template(model);
     }
 }
