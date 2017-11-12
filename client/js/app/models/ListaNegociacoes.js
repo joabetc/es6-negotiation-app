4e408febd1fe6bfb28751bf6a32bd819ea977fe7
Joabe Costa <joabetc@hotmail.com>
2017-11-12 14:57:41 -0200

    Using API Reflection

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,13 +1,15 @@
 class ListaNegociacoes {
 
-  constructor(armadilha) {
+  constructor(context, armadilha) {
     this._negociacoes = [];
     this._armadilha = armadilha;
+    this._context = context;
   }
 
   adciona(negociacao) {
     this._negociacoes.push(negociacao);
-    this._armadilha(this);
+    // this._armadilha(this);
+    Reflect.apply(this._armadilha, this._context, [this]);
   }
 
   get negociacoes() {
@@ -16,7 +18,8 @@ class ListaNegociacoes {
 
   esvazia() {
     this._negociacoes = [];
-    this._armadilha(this);
+    // this._armadilha(this);
+    Reflect.apply(this._armadilha, this._context, [this]);
   }
 
 }
