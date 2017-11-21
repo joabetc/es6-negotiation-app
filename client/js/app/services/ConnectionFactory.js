5830ce4a572c676b1e13d3f1bbc70ac460679bdb
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-11-21 18:07:07 -0200

    Creating stores

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -13,17 +13,27 @@ class ConnectionFactory {
       let openRequest = window.indexedDB.open(dbName, version);
 
       openRequest.onupgradeneeded = e => {
-
+        ConnectionFactory._createStores(e.target.result);
       };
-
+      
       openRequest.onsuccess = e => {
-
+        resolve(e.target.result);
       };
-
+      
       openRequest.onerror = e => {
-
+        console.log(e.target.error);
+        reject(e.target.name);
       };
     });
   }
-
+  
+  static _createStores(connection) {
+    stores.forEach(store => {
+      if (connection.objectStoreNames.contains(store)) 
+        connection.deleteObjectStore(store);
+      
+      connection.createObjectStore(store, { autoIncrement: true });
+    });
+  }
+  
 }
